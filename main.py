939a1b82c2672a3bf93e976fa917a2832fd5b1d1
Erick Santos <erick.sousa@hotmail.com.br>
2022-08-25 22:51:22 -0300

    ✨ adicionado opcao tabela

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from bhaskara import bhaskara
 from time import sleep
+from tabulate import tabulate
+
+a = 0
+b = 0
+c = 0
 
 def coeficiente():
-    a = 0
-    b = 0
-    c = 0
     calculo = {}
     
     print('Informe os valores da equação, f(x) = ax² + bx + c')
@@ -19,6 +21,24 @@ def coeficiente():
     
     sleep(3)
     
+def tabela():
+    x = 0
+    matriz = []
+    
+    print('Informe os valores da função, f(x) = ax² + bx + c')
+    a = int(input('a = '))
+    b = int(input('b = '))
+    c = int(input('c = '))
+    print(f'\nEquação: {a}*{x}² + {b}*{x} + {c}\n')
+    matriz = [["x", f"{a}x² + {b}x + {c}", "f(x)"]]
+    
+    for i in range(-10, 11):
+        fx = a * i**2 + b * i + c
+        linha = [i, f"{a}*({i})² + {b}*({i}) + {c}", fx]
+        matriz.append(linha)
+    
+    tabela = tabulate(matriz, headers="firstrow")
+    print(tabela)
 
 if __name__ == "__main__":
     opcao = 0
@@ -38,7 +58,7 @@ if __name__ == "__main__":
                 case 1:
                     coeficiente()
                 case 2:
-                    print('opcao 2')
+                    tabela()
                 case 0:
                     break
                 case _:
