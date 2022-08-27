3ff61e111085c219461da60e833894c935b69700
Erick Santos <erick.sousa@hotmail.com.br>
2022-08-27 20:39:22 -0300

    ✨ esqueleto da interface grafica

--- app.py
+++ app.py
@@ -1,47 +1,37 @@
 from coeficiente import coeficiente
 from tabela import tabela
+import PySimpleGUI as sg
 
-_a = 0
-_b = 0
-_c = 0
+def main():
+    #Layout
 
-def menu():
-    return int(input('''
-(1) Alterar valores
-(2) Achar coeficientes
-(3) Criar tabela
-(0) Fechar Programa
-> '''))
-
-def pedirValores():
-    global _a
-    global _b
-    global _c
-    print('Informe os valores da função, f(x) = ax² + bx + c')
-    _a = int(input('a = '))
-    _b = int(input('b = '))
-    _c = int(input('c = '))
+    coluna1 = [
+        [sg.Text('Insira os coeficientes:')],
+        [sg.Text('A'), sg.Input(key='-inputA-', size=7), sg.Button('Raizes')],
+        [sg.Text('B'), sg.Input(key='-inputB-', size=7), sg.Button('Grafico')],
+        [sg.Text('C'), sg.Input(key='-inputC-', size=7)],
+        [sg.Text('Resultado:'), sg.Text('')]
+    ]
+    coluna2 = [
+        [sg.Text('Gráfico da Função:')],
+        [sg.Multiline(size=(20,10))]
+    ]
     
+    layout = [
+        [sg.Column(coluna1), sg.VerticalSeparator(), sg.Column(coluna2)]
+    ]
+    
+    #Criação da janela
+    window = sg.Window('Pyrabola', layout)
+    
+    #Loop de eventos
+    while True:
+        event, values = window.read()
+        
+        if event == sg.WIN_CLOSED:
+            break
+    
+    window.close()
 
 if __name__ == "__main__":
-    opcao = 0
-    
-    print('Bem vindo ao Função-inator!')
-    while True:
-        try:
-            opcao = menu()
-        except ValueError:
-            print('Opção Inválida')
-        else:
-            match opcao:
-                case 1:
-                    pedirValores()
-                case 2:
-                    coeficiente(_a, _b, _c)
-                case 3:
-                    tabela(_a, _b, _c)
-                case 0:
-                    print('PROGRAMA ENCERRADO')
-                    break
-                case _:
-                    print('Opção Inválida')
+    main()
